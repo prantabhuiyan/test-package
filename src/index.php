5b445e3fcc9f51103edd9af4ab1445e8407cf9a7
Pranta <pranta@asl.aero>
2020-12-08 12:19:08 +0600

    first commit

---
+++ src/index.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: pranta
+ * Date: 8/12/20
+ * Time: 10:48 AM
+ */
+
+
+namespace pranta\HelloWorld;
+
+class Index{
+    public function greet($greet= "Hello World")
+    {
+        return $greet;
+    }
+}
